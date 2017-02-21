30686abe1699faa45fc2eba5238231ae36ceedb6
BrianHanna <bri.hanna@gmail.com>
2017-02-21 12:41:20 -0500

    InitialCommit
    Initial commit

---
+++ src/org/usfirst/frc1450/FRCSteamworks/commands/driveCommands/TeleopDriveCommand.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc1450.FRCSteamworks.commands.driveCommands;
+
+import org.usfirst.frc1450.FRCSteamworks.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class TeleopDriveCommand extends Command {
+
+    public TeleopDriveCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.drives);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.drives.teleopDrive(Robot.oi.getDriveJoystickXAxis(), Robot.oi.getDriveJoystickYAxis());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
